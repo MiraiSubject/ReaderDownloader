bb1cc7b2f19fed57bf11b4760e899401c0b5a49e
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2024-01-18 21:23:19 +0100

    Fix syntax and exit immediately on non-zero

--- shipit.sh
+++ shipit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # This script will zip up the contents of the dist directory
 # and place it in the root of the project directory
-
+set -e
 function zip_extension() {
     local browser=$1
-    (cd dist && zip -r ../extension-"$browser".zip -x .DS_Store -x "__MACOSX" .)
+    (cd dist && zip -r ../extension-"$browser".zip . -x ".DS_Store" -x "__MACOSX")
 }
 
 pnpm build-firefox
